364c21afc014cb990587e6d5fced37ba44d12911
Yash <ybhanushali1902@gmail.com>
2022-06-25 17:43:15 +0530

    First

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("hardhat-deploy");
+
+module.exports = {
+  // solidity: "0.8.8",
+  solidity: {
+    compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.RINKEBY_RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 4,
+      blockConfirmations: 3,
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gasReport.txt",
+    noColors: true,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    user: {
+      default: 1,
+    },
+  },
+};
